5942fd229dea05c955c74023c6169e78fd583df5
MarijnSt <marijn.stammeleer@gmail.com>
2021-03-11 13:01:18 +0100

    l7

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('test');
+var greet;
+greet = function () {
+    console.log('ieps');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 6);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,12 @@
-console.log('test')
+let greet: Function
+
+greet = () => {
+    console.log('ieps')
+}
+
+const add = (a: number, b: number, c: number | string = 10) => {
+    console.log(a + b)
+    console.log(c)
+}
+
+add(5, 6)
