cebc6902dcfec4753bca72e7ffe5b900ca76f677
MarijnSt <marijn.stammeleer@gmail.com>
2021-03-13 11:59:12 +0100

    l15

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,31 @@
+//interface
+interface isPerson {
+    name: string,
+    age: number,
+    speak (a: string): void
+    spend (a: number): number
+}
+
+const me: isPerson = {
+    name: 'Marijn',
+    age: 28,
+    speak (text: string): void {
+        console.log(text)
+    },
+    spend (amount: number): number {
+        console.log('I spent', amount)
+        return amount
+    }
+}
+
+let diejen: isPerson
+
+const greetPerson = (person: isPerson) => {
+    console.log('Hello', person.name)
+}
+
+greetPerson(me)
+
 import {Invoice} from './classes/Invoice.js'
 
 // objecten aanmaken via class
@@ -10,7 +38,6 @@ invoices.push(invOne)
 invoices.push(invTwo)
 
 invoices.forEach(inv => {
-    // inv.client = 'marijn'
     console.log(inv.client, inv.amount, inv.format())
 })
 
